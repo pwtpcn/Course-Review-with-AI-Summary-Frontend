d00950238c4716836db0e30854aac2d8fe05fbc2
Thanapoom.C <palmdz08@gmail.com>
2026-02-11 16:04:49 +0700

    feat: solve Navbar cant click problem

--- app/routes.ts
+++ app/routes.ts
@@ -4,13 +4,16 @@ export default [
     //สร้างหน้าใหม่แล้วมาเพิ่ม route ที่นี่
     index("routes/home.tsx"),
 
+    //Overall
     route("/login", "routes/login.tsx"),
     route("/jobs", "routes/jobs.tsx"),
-    route("/review", "routes/review.tsx"),
 
-    //user
+    //Review
+    route("/review", "routes/review/review.tsx"),
+
+    //User
     // route("userpage1", "routes/user/userpage1.tsx"),
 
-    //admin
+    //Admin
     // route("adminpage1", "routes/admin/adminpage1.tsx"),
 ] satisfies RouteConfig;
--- app/routes/jobs.tsx
+++ app/routes/jobs.tsx
@@ -1,7 +1,10 @@
+import { Navbar } from "~/component/Navbar";
+
 export default function Jobs() {
   return (
-    <div>
-        Jobs
+    <div className="min-h-screen bg-black text-white font-['Press_Start_2P'] relative overflow-hidden flex flex-col">
+      < Navbar />
+      Jobs
     </div>
   );
 }
